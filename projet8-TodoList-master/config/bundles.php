4521e965f3842dece0c9146174cf7ea062c661c1
Campino <campino.robin@hotmail.fr>
2020-03-25 12:52:33 +0100

    second push
    upgrading to symfony 4

--- projet8-TodoList-master/config/bundles.php
+++ projet8-TodoList-master/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
